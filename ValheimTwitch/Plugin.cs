be1ef6d2b6b3eb995e414441d2fea4267b763c30
skarab42 <skarab@onlfait.ch>
2021-03-11 09:27:34 +0100

    refacto: Const to uppercase

--- ValheimTwitch/Plugin.cs
+++ ValheimTwitch/Plugin.cs
@@ -2,17 +2,17 @@
 
 namespace ValheimTwitch
 {
-    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    [BepInPlugin(GUID, NAME, VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string pluginGUID = "dev.skarab42.valheim_twitch";
-        public const string pluginName = "Valheim Twitch";
-        public const string pluginLabel = "ValheimTwitch";
-        public const string pluginVersion = "0.1.0";
+        public const string GUID = "dev.skarab42.valheim_twitch";
+        public const string NAME = "Valheim Twitch";
+        public const string LABEL = "ValheimTwitch";
+        public const string VERSION = "0.1.0";
 
         void Awake()
         {
-            Log.Info($"{pluginName} Awake!");
+            Log.Info($"{NAME} Awake!");
         }
     }
 }
