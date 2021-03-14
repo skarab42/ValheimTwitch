0d4f114552ed9946acc9e6aeb5c82f844177b8ca
skarab42 <skarab@onlfait.ch>
2021-03-14 16:33:01 +0100

    refacto: Remove unused code

--- ValheimTwitch/Helpers/EmbeddedAsset.cs
+++ ValheimTwitch/Helpers/EmbeddedAsset.cs
@@ -10,7 +10,6 @@ namespace ValheimTwitch.Helpers
         public static Stream LoadEmbeddedAsset(string assetPath)
         {
             Assembly objAsm = Assembly.GetExecutingAssembly();
-            string[] embeddedResources = objAsm.GetManifestResourceNames();
 
             if (objAsm.GetManifestResourceInfo(objAsm.GetName().Name + "." + assetPath) != null)
             {
