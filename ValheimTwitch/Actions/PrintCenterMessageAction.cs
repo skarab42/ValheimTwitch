a72caf3e7f9cc1e4012c1239e5bf09034af376da
skarab42 <skarab@onlfait.ch>
2021-03-19 18:17:19 +0100

    feat: Print HUD messages (top-left/center)

--- ValheimTwitch/Actions/PrintCenterMessageAction.cs
+++ ValheimTwitch/Actions/PrintCenterMessageAction.cs
@@ -1,6 +1,28 @@
-﻿namespace ValheimTwitch.Events
+﻿using System;
+using ValheimTwitch.Twitch.PubSub.Messages;
+
+namespace ValheimTwitch.Events
 {
     internal class PrintCenterMessageAction : Action
     {
+        public override void Run(Redemption redemption)
+        {
+            try
+            {
+                var user = redemption.User.DisplayName;
+                var text = redemption.UserInput;
+
+                Log.Info($"Message -> user:{user} text:{text}");
+
+                if (Player.m_localPlayer != null)
+                {
+                    Player.m_localPlayer.Message(MessageHud.MessageType.Center, $"<{user}> {text}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+        }
     }
 }
--- ValheimTwitch/Actions/PrintTopLeftMessageAction.cs
+++ ValheimTwitch/Actions/PrintTopLeftMessageAction.cs
@@ -1,6 +1,28 @@
-﻿namespace ValheimTwitch.Events
+﻿using System;
+using ValheimTwitch.Twitch.PubSub.Messages;
+
+namespace ValheimTwitch.Events
 {
     internal class PrintTopLeftMessageAction : Action
     {
+        public override void Run(Redemption redemption)
+        {
+            try
+            {
+                var user = redemption.User.DisplayName;
+                var text = redemption.UserInput;
+
+                Log.Info($"Message -> user:{user} text:{text}");
+
+                if (Player.m_localPlayer != null)
+                {
+                    Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"<{user}> {text}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+        }
     }
 }
