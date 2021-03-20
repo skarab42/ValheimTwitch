5ace528fc3cd1ae24a0e4abf8ff65d8b5940f52b
skarab42 <skarab@onlfait.ch>
2021-03-20 17:23:41 +0100

    draft: GUI prefab

--- ValheimTwitch/Patches/FejdStartupPatch.cs
+++ ValheimTwitch/Patches/FejdStartupPatch.cs
@@ -1,4 +1,8 @@
-﻿using HarmonyLib;
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
 using ValheimTwitch.GUI;
 
 namespace ValheimTwitch.Patches
@@ -16,6 +20,34 @@ namespace ValheimTwitch.Patches
             UpdateMainButonText();
 
             startup.startGui.mainButton.button.onClick.AddListener(OnMainButtonClick);
+
+            var myLoadedAssetBundle
+            = AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "ValheimTwitch", "valheimtwitchgui"));
+            if (myLoadedAssetBundle == null)
+            {
+                Debug.Log("Failed to load AssetBundle!");
+                return;
+            }
+
+            var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Valheim Twitch GUI");
+
+            if (!prefab)
+            {
+                Log.Info($"Prefab not found!!!");
+            }
+            else
+            {
+                Log.Info($"Prefab ----> {prefab.name}");
+                var gui = Object.Instantiate(prefab);
+
+                var button = gui.GetComponentInChildren<Button>();
+
+                button.onClick.AddListener(() =>
+                {
+                    Log.Info("Prout prout prout !");
+                });
+            }
+
         }
 
         private static void OnMainButtonClick()
