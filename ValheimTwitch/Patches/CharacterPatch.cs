a25bc2144251aec5e988eeda30571e59dfd46747
skarab42 <skarab@bluewin.ch>
2021-04-06 11:08:27 +0200

    feat: Spawn Tamed creature which takes the name of the viewer (#9)

---
+++ ValheimTwitch/Patches/CharacterPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using ValheimTwitch.Helpers;
+
+namespace ValheimTwitch.Patches
+{
+    [HarmonyPatch(typeof(Character), "Awake")]
+    public static class CharacterAwakePatch
+    {
+        public static void Postfix(ref Character __instance, ref ZNetView ___m_nview)
+        {
+            var zdo = ___m_nview.GetZDO();
+
+            if (zdo == null)
+                return;
+
+            var customName = zdo.GetString($"{Plugin.GUID}-name");
+            var customTamed = zdo.GetBool($"{Plugin.GUID}-tamed");
+
+            if (customName.Length > 0)
+                __instance.m_name = customName;
+            
+            if (customTamed)
+                Prefab.SetTameable(___m_nview, __instance.gameObject);
+        }
+    }
+}
+
